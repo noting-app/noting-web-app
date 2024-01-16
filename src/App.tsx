8da6be68a581eb7879f6e4a8e18ef47f14e30a67
Arsenii Izmailov <67082372+Turbocat6413@users.noreply.github.com>
2024-01-16 23:13:14 +0300

    Added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
+import { Route, Router } from '@solidjs/router'
 import './App.css'
+import Main from './routes/main'
 
 function App() {
-
-  
-  return (
-    <>
-      noting
-    </>
-  )
+	return (
+		<>
+			<Router>
+				<Route path="/" component={Main} />
+				<Route path="*" component={Main} />
+			</Router>
+		</>
+	)
 }
 
 export default App
---
+++ src/routes/main/index.tsx
@@ -0,0 +1,9 @@
+import './styles.css'
+
+export default function Main() {
+    return (
+        <div>
+            <h1>Main</h1>
+        </div>
+    )
+}
